1189d816a9a2a2d1da22b879950d34fb57a5bd56
Arion <Arion@DESKTOP-9BU80RS>
2023-12-01 22:59:45 +0200

    Добавьте файлы проекта.

---
+++ laba52/Program.cs
@@ -0,0 +1,6 @@
+﻿//средний 19
+Console.WriteLine("Введите строку символов:");
+string inputString = Console.ReadLine();
+string resultString = string.Join(" ", inputString.ToCharArray());
+Console.WriteLine("Результат:");
+Console.WriteLine(resultString);
